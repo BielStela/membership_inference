7b90b1c1ef9fcad111e6183188f794082cef5022
BielStela <biel.stela@gmail.com>
2018-11-23 15:38:06 +0100

    WIP shit

--- shadow_model.py
+++ shadow_model.py
@@ -1,8 +1,10 @@
 from typing import List, Tuple, Dict
 
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.base import clone
+
+import pandas as pd
 
 
 class ShadowModels:
@@ -11,7 +13,7 @@ class ShadowModels:
     of the synthetic data.
 
     TODO:
-        - Split the synth data in n_models parts
+        - Split the synth data in n_models parts with labels equally distributed
         - The splited data is splited again in train and test (1/2)
         - Instance n models (must be similar to the target model)
         - Train each model on training splited data
@@ -21,7 +23,7 @@ class ShadowModels:
         - drop mic
     """
 
-    def __init__(self, n_models: int, data: np.ndarray,
+    def __init__(self, n_models: int, data: tuple,
                  target_classes: int, learner):
 
         self.n_models = n_models
@@ -33,10 +35,19 @@ class ShadowModels:
 
     @staticmethod
     def _split_data(data, n_splits, shuffle=False) -> List[np.ndarray]:
-        if shuffle:
-            np.random.shuffle(data)  # shufle data just in case
-        splited = np.vsplit(data, n_splits)
-        return splited
+        X, y = data
+
+        classes = np.unique(y)
+        n_classes = len(classes)
+
+        for cl in classes:
+
+            X_cls = X[y == cl, :]
+            batch_size = len(X_cls)//n_splits
+            
+            for i in range(n_splits):
+                split_X = X_cls[i*batch_size:(i+1)*batch_size, :]
+                split_y = y[i:(i+1)*batch_size]
 
     @staticmethod
     def _get_model_list(learner, n) -> List:
@@ -46,7 +57,7 @@ class ShadowModels:
         models = [clone(learner) for _ in range(n)]
         return models
 
-    def fit(self):
+    def train_shadow_models(self):
 
         # TRAIN
         for model, data_subset in zip(self.models, self.splits):
