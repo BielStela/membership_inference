3fdfb00df78a5dcdff92991270ecf044323d6e08
Biel Stela <biel.stela@gmail.com>
2019-02-05 11:42:36 +0100

    more notebooks

---
+++ mblearn/base_learner.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Learner(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def predict_proba(self):
+        pass
